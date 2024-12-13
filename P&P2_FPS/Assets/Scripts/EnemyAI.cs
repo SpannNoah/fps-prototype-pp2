5f8709c27ac338a3a15e1dfb12d33c8fe8c65f2a
SpannNoah <162138804+SpannNoah@users.noreply.github.com>
2024-12-12 19:52:24 -0600

    first half lecture 3

--- P&P2_FPS/Assets/Scripts/EnemyAI.cs
+++ P&P2_FPS/Assets/Scripts/EnemyAI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -13,6 +14,8 @@ public class EnemyAI : MonoBehaviour, IDamage
     private Transform m_headPos = null;
     [SerializeField]
     private NavMeshAgent m_navMeshAgent = null;
+    [SerializeField]
+    private Animator m_animator = null;
 
     [SerializeField]
     private int m_health = 100;
@@ -26,16 +29,29 @@ public class EnemyAI : MonoBehaviour, IDamage
     float m_fireRate = 10.0f;
     [SerializeField]
     private int m_fieldOfView = 90;
+    [SerializeField]
+    private int m_roamDistance = 10;
+    [SerializeField]
+    private int m_roamTimer = 3;
+    [SerializeField]
+    private int m_speedTransition = 3;
 
     private bool m_isShooting = false;
     private bool m_isPlayerInRange = false;
     private Color m_colorOriginal = Color.white;
     private Vector3 m_playerDirection = Vector3.zero;
     private float m_angleToPlayer = 0f;
+    private bool m_isRoaming = false;
+    private Vector3 m_startingPos = Vector3.zero;
+    private float m_originalStoppingDist = 0.0f;
+    private Coroutine m_coroutine = null;
+    
     // Start is called before the first frame update
     void Start()
     {
         m_colorOriginal = m_model.material.color;
+        m_startingPos = transform.position;
+        m_originalStoppingDist = m_navMeshAgent.stoppingDistance;
         if(GameManager.Instance != null)
         {
             GameManager.Instance.UpdateGameGoal(1);
@@ -44,12 +60,45 @@ public class EnemyAI : MonoBehaviour, IDamage
 
     // Update is called once per frame
     void Update()
-    { 
+    {
+        float agentSpeed = m_navMeshAgent.velocity.normalized.magnitude;
+        float animSpeed = m_animator.GetFloat("Speed");
+        
+        
+        m_animator.SetFloat("Speed", Mathf.MoveTowards(animSpeed, agentSpeed, Time.deltaTime * m_speedTransition));
+        
         m_navMeshAgent.SetDestination(GameManager.Instance.m_player.transform.position);
-        if (m_isPlayerInRange && CanSeePlayer())
+        if (m_isPlayerInRange && !CanSeePlayer())
         {
-            
+            if(!m_isRoaming && m_navMeshAgent.remainingDistance < .01f)
+            {
+                m_coroutine = StartCoroutine(RoamCoroutine());
+            }
         }
+        else if(!m_isPlayerInRange)
+        {
+            if(!m_isRoaming && m_navMeshAgent.remainingDistance < .01f)
+            {
+                m_coroutine = StartCoroutine(RoamCoroutine());
+            }
+        }
+    }
+
+    private IEnumerator RoamCoroutine()
+    {
+        m_isRoaming = true;
+        yield return new WaitForSeconds(m_roamTimer);
+        
+        m_navMeshAgent.stoppingDistance = 0;
+        Vector3 randPos = Random.insideUnitSphere * m_roamDistance;
+
+        randPos += m_startingPos;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randPos, out hit, m_roamDistance, 1);
+        m_navMeshAgent.SetDestination(hit.position);
+        
+        m_isRoaming = false;
     }
 
     public bool CanSeePlayer()
@@ -77,11 +126,13 @@ public class EnemyAI : MonoBehaviour, IDamage
                     StartCoroutine(Shoot());
                 }
 
+                m_navMeshAgent.stoppingDistance = m_originalStoppingDist;
                 return true;
             }
 
         }
 
+        m_navMeshAgent.stoppingDistance = 0;
         return false;
     }
     
@@ -89,6 +140,8 @@ public class EnemyAI : MonoBehaviour, IDamage
     {
         m_health -= amount;
         m_navMeshAgent.SetDestination(GameManager.Instance.m_player.transform.position);
+        StopCoroutine(m_coroutine);
+        m_isRoaming = false;
         StartCoroutine(DamageFlashCoroutine());
 
         if(m_health <= 0)
@@ -128,6 +181,7 @@ public class EnemyAI : MonoBehaviour, IDamage
         if (other.CompareTag("Player"))
         {
             m_isPlayerInRange = false;
+            m_navMeshAgent.stoppingDistance = 0;
         }
     }
 
