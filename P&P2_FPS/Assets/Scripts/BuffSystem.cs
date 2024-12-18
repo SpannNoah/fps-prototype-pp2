3dac90bccfd55dc97e21ed779932849635899a27
Christopher Wilt <ctwilt23@gmail.com>
2024-12-18 01:05:17 -0500

    Fix Player Controller

--- P&P2_FPS/Assets/Scripts/BuffSystem.cs
+++ P&P2_FPS/Assets/Scripts/BuffSystem.cs
@@ -3,70 +3,56 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "BuffSystem", menuName = "Buffs/Buff Stats")]
-
-public class BuffSystem : ScriptableObject
+public class BuffSystem : MonoBehaviour
 {
-    public string buffName;
-    public float healthMult;
-    public float speedMult;
-    public float duration;
-    public bool healthResotration;
-    public bool isOverShield;
+    [Header("Buff Settings")]
+    [SerializeField] private float healthMult = 2f;
+    [SerializeField] private float speedMult = 2f;
+    [SerializeField] private float sprintMult = 5f;
+    [SerializeField] private float buffDuration = 6f;
 
-    public enum BuffType
+    private PlayerController playerController;
+    private bool isBuffActive = false;
+    private float origSpeed;
+    private float origSprintMod;
+
+    private void Start()
     {
-        Health, Speed, Duration, HealthRestoration
+        playerController = GameManager.Instance.m_playerController;
     }
 
-    //[Header("Buff Settings")]
-    //[SerializeField] private float healthMult = 2f;
-    //[SerializeField] private float speedMult = 2f;
-    //[SerializeField] private float sprintMult = 5f;
-    //[SerializeField] private float buffDuration = 6f;
+    public void ApplyBuff()
+    {
+        if (!isBuffActive)
+        {
+            isBuffActive = true;
+            StartCoroutine(BuffCoroutine());
+        }
+    }
 
-    //private PlayerController playerController;
-    //private bool isBuffActive = false;
-    //private float origSpeed;
-    //private float origSprintMod;
+    private IEnumerator BuffCoroutine()
+    {
+        origSpeed = playerController.m_baseSpeed;
+        origSprintMod = playerController.m_baseSprintModifier;
 
-    //private void Start()
-   // {
-    //    playerController = GameManager.Instance.m_playerController;
-  //  }
+        playerController.SetHealth((int)(playerController.Health * healthMult));
+        playerController.SetSpeed(origSpeed * speedMult);
+        playerController.SetSprintModifier(origSprintMod * sprintMult);
+        Debug.Log("Buff Applied");
 
-    //public void ApplyBuff()
-    //{
-    //    if (!isBuffActive)
-    //    {
-    //        isBuffActive = true;
-    //        StartCoroutine(BuffCoroutine());
-    //    }
-    //}
+        yield return new WaitForSeconds(buffDuration);
 
-    //private IEnumerator BuffCoroutine()
-    //{
-    //    origSpeed = playerController.m_baseSpeed;
-    //    origSprintMod = playerController.m_baseSprintModifier;
+        RemoveBuff(origSpeed, origSprintMod);
 
-    //    playerController.SetHealth((int)(playerController.Health * healthMult));
-    //    playerController.SetSpeed(origSpeed * speedMult);
-    //    playerController.SetSprintModifier(origSprintMod * sprintMult);
-    //    Debug.Log("Buff Applied");
+    }
 
-    //    yield return new WaitForSeconds(buffDuration);
-
-    //    RemoveBuff(origSpeed, origSprintMod);
-
-    //}
-
-    //private void RemoveBuff(float origSpeed, float origSprintMod)
-    //{
-    //    playerController.SetHealth(playerController.playerHealthOrig);
-    //    playerController.SetSpeed(origSpeed);
-    //    playerController.SetSprintModifier(origSprintMod);
-    //    isBuffActive = false;
-    //    Debug.Log("Buff Removed");
-    //}
+    private void RemoveBuff(float origSpeed, float origSprintMod)
+    {
+        playerController.SetHealth(playerController.playerHealthOrig);
+        playerController.SetSpeed(origSpeed);
+        playerController.SetSprintModifier(origSprintMod);
+        isBuffActive = false;
+        Debug.Log("Buff Removed");
+    }
 
 }
