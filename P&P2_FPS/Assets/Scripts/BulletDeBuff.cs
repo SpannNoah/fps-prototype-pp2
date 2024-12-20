db4d7e28186683aca2054fdd901afe630838f4a3
Colton Schaubschlager <CJSchaubschlager@student.fullsail.edu>
2024-12-19 17:59:48 -0800

    added a bullet debuff to apply debuffs to enemyAI bullets

---
+++ P&P2_FPS/Assets/Scripts/BulletDeBuff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class  BulletDeBuff : MonoBehaviour
+{
+    [SerializeField] private scriptableDeBuff deBuff;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.m_playerController.ApplyDeBuff(deBuff);
+            Destroy(gameObject);
+        }
+    }
+}
