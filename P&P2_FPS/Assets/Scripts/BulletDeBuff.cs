ba96912ca7398ccb97eb6107cd281ab8f17449a5
Colton Schaubschlager <CJSchaubschlager@student.fullsail.edu>
2024-12-19 18:08:30 -0800

    Updated bullet Debuff script to apply debuffs at random and with a random chance

--- P&P2_FPS/Assets/Scripts/BulletDeBuff.cs
+++ P&P2_FPS/Assets/Scripts/BulletDeBuff.cs
@@ -6,12 +6,22 @@ using UnityEngine;
 public class  BulletDeBuff : MonoBehaviour
 {
     [SerializeField] private scriptableDeBuff deBuff;
+    [SerializeField]
+    [Range(0, 1f)] private float applyChance = 0.5f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            GameManager.Instance.m_playerController.ApplyDeBuff(deBuff);
+            float randomValue = UnityEngine.Random.Range(0, 1f);
+            if (randomValue <= applyChance)
+            {
+                GameManager.Instance.m_playerController.ApplyDeBuff(deBuff);
+            }
+            else
+            {
+                Debug.Log("Debuff not applied");
+            }
             Destroy(gameObject);
         }
     }
