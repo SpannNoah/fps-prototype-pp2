6bb3e99a53bbf3ec66a38101c6a3255ba21cace2
Colton Schaubschlager <CJSchaubschlager@student.fullsail.edu>
2025-01-25 15:41:13 -0800

    Added a rotate object script and a spawner script

---
+++ P&P2_FPS/Assets/Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject objectToSpawn;
+    [SerializeField] int numToSpawn;
+    [SerializeField] int timeBetweenSpawns;
+    [SerializeField] float spawnRadius;
+    [SerializeField] Transform[] spawnPos;
+
+    int spawnCount;
+
+    bool StartSpawning;
+    bool isSpawning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.UpdateGameGoal(numToSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StartSpawning && spawnCount < numToSpawn && !isSpawning)
+        {
+            StartCoroutine(Spawn());
+        }
+    }
+
+    IEnumerator Spawn()
+    {
+        isSpawning = true;
+        yield return new WaitForSeconds(timeBetweenSpawns);
+
+        int spawnInt = Random.Range(0, spawnPos.Length);
+
+        Instantiate(objectToSpawn, spawnPos[spawnInt].position, Quaternion.identity);
+        spawnCount++;
+
+        isSpawning = false;
+
+    }
+}
