658599fc0eb748d9e70dc49767e2b792662c3f30
Johnathan Spann <JNSpann@student.fullsail.edu>
2025-01-25 21:15:45 -0600

    modifies color on overheat meter depending on percent fill

--- P&P2_FPS/Assets/Scripts/OverheatMeter.cs
+++ P&P2_FPS/Assets/Scripts/OverheatMeter.cs
@@ -24,6 +24,16 @@ public class OverheatMeter : MonoBehaviour
     {
         Debug.Log("Heat Increasing by " + amount);
         m_overheatMeter.fillAmount += amount;
+
+        if(m_overheatMeter.fillAmount >= .99)
+        {
+            m_overheatMeter.color = Color.red;
+        }
+
+        if(m_overheatMeter.fillAmount <= .01)
+        {
+            m_overheatMeter.color = Color.white;
+        }
         //Mathf.Clamp01(amount);
     }
 
